cfdb55a18fe8e72ee6afe3313339a15428058a0c
Tim Schindler <xh3b4sd@gmail.com>
2016-08-14 23:07:04 +0000

    modifications for spec.NetworkPayload

--- template.go
+++ template.go
@@ -18,6 +18,7 @@ var generatedGo = `package {{.PackageName}}
 	import (
 		"reflect"
 
+		"github.com/xh3b4sd/anna/api"
 		"github.com/xh3b4sd/anna/factory/id"
 		"github.com/xh3b4sd/anna/log"
 		"github.com/xh3b4sd/anna/spec"
@@ -104,18 +105,21 @@ var generatedGo = `package {{.PackageName}}
 	}
 
 	func (c *clg) Calculate(payload spec.NetworkPayload) (spec.NetworkPayload, error) {
-		outputs, err := filterError(reflect.ValueOf(c.calculate).Call(payload.Args))
+		outputs, err := filterError(reflect.ValueOf(c.calculate).Call(payload.GetArgs()))
 		if err != nil {
-			return spec.NetworkPayload{}, maskAny(err)
+			return nil, maskAny(err)
 		}
 
-		calculatedPayload := spec.NetworkPayload{
-			Args:        outputs,
-			Destination: "", // This has to be decided by Network.Forward.
-			Sources:     []spec.ObjectID{payload.Destination},
+		newNetworkPayloadConfig := api.DefaultNetworkPayloadConfig()
+		newNetworkPayloadConfig.Args = outputs
+		newNetworkPayloadConfig.Destination = "must be set by spec.Network.Forward"
+		newNetworkPayloadConfig.Sources = []spec.ObjectID{payload.GetDestination()}
+		newNetworkPayload, err := api.NewNetworkPayload(newNetworkPayloadConfig)
+		if err != nil {
+			return nil, maskAny(err)
 		}
 
-		return calculatedPayload, nil
+		return newNetworkPayload, nil
 	}
 
 	func (c *clg) GetName() string {
